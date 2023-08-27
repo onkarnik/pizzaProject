a0e9d33f23cbab2ae4461e7e83f9d3429457c0b3
onkar nikam <onkarnik@gmail.com>
2023-08-27 21:54:58 +0530

    cors added for userController

--- Pizza Ordering System/BackEnd/spring_boot_backend/src/main/java/com/app/controller/UserController.java
+++ Pizza Ordering System/BackEnd/spring_boot_backend/src/main/java/com/app/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.app.entity.User;
 import com.app.service.UserService;
 
 @RestController("/user")
+@CrossOrigin(origins = "*")
 public class UserController {
 	
 	@Autowired
