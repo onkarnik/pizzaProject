8b47c4e7faea433b01c255286f793fa5a12d4eaa
Prashant <tomarprashant@gmail.com>
2023-08-27 23:21:30 +0530

    changes in index

--- Pizza Ordering System/FrontEnd/src/Screens/index.js
+++ Pizza Ordering System/FrontEnd/src/Screens/index.js
@@ -26,11 +26,10 @@ function Controller() {
           <ProtectedRoute exact path="/pizza" component={Card} />
           <ProtectedRoute exact path="/checkout" component={CheckoutForm} />
           <ProtectedRoute exact path="/cart" component={Cart} />
-          <Route exact path="/login" component={Login} 
-          <Route exact path="/registerUser" component={RegisterUser}/>
+          <Route exact path="/login" component={Login} />
           <Route exact path ="/RegisterUser" component={RegisterUser}/>
-        </Switch>
-         <Footer />
+         </Switch>
+        <Footer/>
       </BrowserRouter>
       
     </>
