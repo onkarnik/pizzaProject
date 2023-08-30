5ff2ca1852cc714b61821be8e953a5d672e4c1b8
onkar nikam <onkarnik@gmail.com>
2023-08-30 11:31:06 +0530

    upload pizza image in backend done

--- Pizza Ordering System/BackEnd/spring_boot_backend/src/main/java/com/app/DTO/PizzaDTO.java
+++ Pizza Ordering System/BackEnd/spring_boot_backend/src/main/java/com/app/DTO/PizzaDTO.java
@@ -29,7 +29,7 @@ public class PizzaDTO {
 	@NotBlank
     private String category;
 	
-	@NotBlank
+	
     private String image;
 	
 	@NotBlank
---
+++ Pizza Ordering System/BackEnd/spring_boot_backend/src/main/java/com/app/service/ImageHandlingService.java
@@ -0,0 +1,11 @@
+package com.app.service;
+
+import java.io.IOException;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.app.DTO.ApiResponse;
+
+public interface ImageHandlingService {
+	ApiResponse uploadImage(Long pizzaId, MultipartFile image) throws IOException;
+}
